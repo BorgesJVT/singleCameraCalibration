5379a2302403d964f0fc60f61e762e9fb196d178
João Victor Torres Borges <jvtb@ic.ufal.br>
2017-03-16 17:01:39 -0300

    first commit

---
+++ build/CMakeFiles/singleCameraCalibration.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/singleCameraCalibration.dir/src/singleCameraCalibration.cpp.o"
+  "singleCameraCalibration.pdb"
+  "singleCameraCalibration"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/singleCameraCalibration.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ include/Config.h
@@ -0,0 +1,3 @@
+// Preprocessor directives to set variables from values in the CMakeLists.txt files
+#define DATA_FOLDER "/home/joao/Documentos/ComputerVisionProjects/SingleCameraCalibration/data/"
+#define IMAGE_FOLDER "/home/joao/Documentos/ComputerVisionProjects/SingleCameraCalibration/data/images/"
